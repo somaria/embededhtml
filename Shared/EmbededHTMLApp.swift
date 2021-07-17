95414270c85441989ab9fe0305992ce38b9227be
Daryl <daryl.9@gmail.com>
2021-07-17 10:07:46 +0800

    Initial Commit

---
+++ Shared/EmbededHTMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbededHTMLApp.swift
+//  Shared
+//
+//  Created by admin on 17/7/21.
+//
+
+import SwiftUI
+
+@main
+struct EmbededHTMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
